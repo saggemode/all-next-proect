a46fd24da8bdea9fbb07b1a7225331126c548904
saggemode <saggemode@gmail.com>
2023-07-06 00:40:30 +0100

    hello

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+// import 'antd/dist/reset.css';
+import { Provider } from "react-redux";
+import store from "../store/store";
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
